358a6bccbf47f7532cb82271d1a7a3e18f6f916f
Lucas Seixas <lucasseixaslima@yahoo.com.br>
2024-01-08 20:41:05 -0300

    title e big card component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HomeTitleComponent } from '../../components/home-title/home-title.component';
+import { BigCardComponent } from '../../components/big-card/big-card.component';
+import { SmallCardComponent } from '../../components/small-card/small-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HomeTitleComponent,BigCardComponent,SmallCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
